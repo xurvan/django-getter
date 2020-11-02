772edee21302b44fe92591c6136049eb11df74e9
Xurvan <xurvan@tuta.io>
2020-11-02 19:15:05 +0330

    Add "get_model"

--- getter/__init__.py
+++ getter/__init__.py
@@ -13,3 +13,19 @@ def get_setting(attr: str, *fields):
             return None
 
     return value
+
+
+def get_model(model, app_label=None):
+    if app_label:
+        if app_label not in apps.all_models:
+            raise ValueError(f'App not found ({app_label})')
+        if model in apps.all_models[app_label]:
+            return apps.all_models[app_label][model]
+
+        raise KeyError(f'Model not found ({model} in {app_label})')
+
+    for app in apps.all_models:
+        if model in apps.all_models[app]:
+            return apps.all_models[app][model]
+
+    raise KeyError(f'Model not found ({model})')
